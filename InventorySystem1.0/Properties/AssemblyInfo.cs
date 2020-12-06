63ece25143c532d3218782ead91a52005498137e
Kristiyan <37977687+lilprinter@users.noreply.github.com>
2020-12-07 00:44:08 +0200

    Functions

---
+++ InventorySystem1.0/Properties/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("БИЗЗ")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("БИЗЗ")]
+[assembly: AssemblyCopyright("Copyright ©  2020 leff & print")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ca97507a-5b5d-495e-9a8a-7948598fcb78")]
+
+[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyFileVersion("0.0.0.1")]
---
+++ readme.md
@@ -0,0 +1,17 @@
+Features:
+ 
+Login System
+Manage Stock master, out, return
+Generate report
+Settings
+User management system
+How To Run??
+Start Apache and MySQL in XAMPP, and follow the given steps
+
+Connecting Database (MySQL)
+
+1st Step: Open a browser and go to URL “http://localhost/phpmyadmin/”
+2nd Step: Then, click on the databases tab
+3rd Step: Create database naming “ db_inventory ” and then click on the Import tab
+4th Step: Click on browse file and select “db_inventory.sql” file which is inside “Database (MySQL)” folder
+5th Step: Click on go.
